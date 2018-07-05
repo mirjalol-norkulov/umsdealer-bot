0bd76035732ef8e1a2ccade19831d4ae31b72ed4
mirjalol <mirjalol2401@gmail.com>
2018-07-05 18:01:44 +0500

    added gunicorn to requirements

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ import os
 import config
 from flask import Flask, request
 
-server = Flask(__name__)
+app = Flask(__name__)
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
-@server.route("/bot", methods=['POST'])
+@app.route("/bot", methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
 
-@server.route("/")
+@app.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url="https://umsdealer-bot.herokuapp.com/bot")
