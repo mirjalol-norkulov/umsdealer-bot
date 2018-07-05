60ea71c8f9b2af1d765ce3e2432136feb5560b74
mirjalol <mirjalol2401@gmail.com>
2018-07-05 17:55:49 +0500

    app.py and Procfile corrected

--- app.py
+++ app.py
@@ -1,74 +1,21 @@
-import flask
 import telebot
-import logging
-import time
-
+import os
 import config
+from flask import Flask, request
 
-# from main import bot
-
-API_TOKEN = config.TOKEN
+server = Flask(__name__)
 
 bot = telebot.TeleBot(config.TOKEN)
 
-WEBHOOK_HOST = 'https://umsdealer-bot.herokuapp.com/'
-WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
-
-# Quick'n'dirty SSL certificate generation:
-#
-# openssl genrsa -out webhook_pkey.pem 2048
-# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
-#
-# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
-# with the same value in you put in WEBHOOK_HOST
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
-
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
-
-app = flask.Flask(__name__)
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, text="Welcome!")
-
-
-# Empty webserver index, return nothing, just http 200
-@app.route('/', methods=['GET', 'HEAD'])
-def index():
-    return '200 Ok!'
-
-
-# Process webhook calls
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+@server.route("/")
 def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        flask.abort(403)
-
-
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
-
-time.sleep(0.1)
-
-# Set webhook
-bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-                certificate=open(WEBHOOK_SSL_CERT, 'r'))
-
-# Start flask server
-# app.run(host=WEBHOOK_LISTEN,
-#         port=WEBHOOK_PORT,
-#         # ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-#         debug=True)
+    bot.remove_webhook()
+    bot.set_webhook(url="https://umsdealer-bot.herokuapp.com/bot")
+    return "!", 200
